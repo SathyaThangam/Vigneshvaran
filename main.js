45eb85f5b3b551b8c1325d1cb8381596d09ed077
kvigneshvaran99 <vigneshvaran@codingmart.com>
2019-12-30 10:44:39 +0530

    2nd commit

---
+++ main.js
@@ -0,0 +1,272 @@
+var obj=[
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fcocoon.png?alt=media&token=16f8ce3a-f9cc-490f-9ccc-0c48c95d29b5",
+        heading:"Cocoon",
+        sub:"A Space for Family",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fcocoon_v1.3.0_2.PNG?alt=media&token=db2a15e8-8455-46d1-9bae-6bbfea9b3f8b",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fcocoon_v1.3.0_3.PNG?alt=media&token=ea58096e-cd81-4e0e-9c03-a34e400cb5fb",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fcocoon_v1.3.0_4.PNG?alt=media&token=cbd61378-2a14-46eb-ab5c-e662860a8e1f",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fcocoon_v1.3.0_5.PNG?alt=media&token=f37f5450-0d10-48eb-9f15-27ff4d965abe"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fhoneylogo.jpg?alt=media&token=1edee6d0-a5c6-4277-a0ca-c0ad841e295c",
+        heading:"Honey",
+        sub:"Smart Shopping Assistant",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fhoney_v1.13.0_2.PNG?alt=media&token=4eca70a1-32b9-476e-9fbd-7c902d659315",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fhoney_v1.13.0_3.PNG?alt=media&token=0a412a83-5739-49fd-a7d2-cd62604dd5c3",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fhoney_v1.13.0_4.PNG?alt=media&token=6177110b-2297-426c-8202-65e0acb890ea",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fhoney_v1.13.0_5.PNG?alt=media&token=780647cc-7931-4e99-9a03-4b659c71f180"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fkitchen%20stories%202.jpg?alt=media&token=08154c9f-a150-48da-a898-a66821cd66ba",
+        heading:"Kitchen Stories",
+        sub:"Thousands of dishes to enjoy",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fkitchen_stories_v14.1.1_1.PNG?alt=media&token=31f5d60e-c138-4eb1-a539-169ca1cc05df",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fkitchen_stories_v14.1.1_2.PNG?alt=media&token=6694e9f2-768e-48b7-81e8-25e1ac9b66d5",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fkitchen_stories_v14.1.1_4.PNG?alt=media&token=ebe55908-477a-47f0-864e-889ac4487010",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fkitchen_stories_v14.1.1_6.PNG?alt=media&token=f52e2189-c647-439d-84f0-d9c5f300571e"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fthreads%202.jpg?alt=media&token=6b00203f-7c91-4ca4-acff-08144eee0c23",
+        heading:"Threads",
+        sub:"A Camera App for Close Friends",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fthreads_v1.0_1.PNG?alt=media&token=8d1b9b6b-e19f-41dc-bf4d-12dd16e6cf3d",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fthreads_v1.0_2.PNG?alt=media&token=53f90c8b-e11b-4522-88a2-32f9c41016f8",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fthreads_v1.0_3.PNG?alt=media&token=ea2050e4-e9b5-42be-843e-0fddacc5d61c",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fthreads_v1.0_4.PNG?alt=media&token=f3cd6b11-ee7d-40f5-b47b-2fdeb823078b"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fuber_v3.316.jpg?alt=media&token=33f40ba6-9c5c-44e6-8c00-6a9938e5b11d",
+        heading:"Uber",
+        sub:"Know the cost before riding.",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fuber_v3.369_2.PNG?alt=media&token=e5de1552-b7e5-4a7b-bb0c-21ddca53e95b",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fuber_v3.369_3.PNG?alt=media&token=2d44dfdf-ab69-419e-a3c8-401cad82110d",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fuber_v3.369_4.PNG?alt=media&token=0f532453-6da0-47ea-b36c-54fc41a4e9d9",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fuber_v3.369_6.PNG?alt=media&token=19c8550f-5ae4-4034-82e4-7b5ff5261772"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fiftttlogo.jpg?alt=media&token=2eb764d9-9d44-44bc-8182-85cf9fca2635",
+        heading:"IFTTT",
+        sub:"Connect every thing",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fifttt_v4.2.0_0.PNG?alt=media&token=5a1b845a-2fad-4b0f-acb8-6568ebf1d2ec",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fifttt_v4.2.0_2.PNG?alt=media&token=b99cf581-53fb-4883-aadc-832a985895eb",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fifttt_v4.2.0_3.PNG?alt=media&token=c0bbe0be-2817-4b0d-9534-445a05adb265",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fifttt_v4.2.0_4.PNG?alt=media&token=85954285-db2c-4b76-aa35-97a88fe408ab"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fshakeshack.jpg?alt=media&token=eb7ea925-459a-4d9f-8dd7-e5147e037434",
+        heading:"Shake Shack",
+        sub:"Order ahead and cut the line",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fshakeshack_v4.1.7_2.PNG?alt=media&token=11e9dd8c-9625-4b34-9db9-1ff240631c1f",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fshakeshack_v4.1.7_3.PNG?alt=media&token=c5954530-c2a9-4bee-8dfc-0c781d26acbf",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fshakeshack_v4.1.7_4.PNG?alt=media&token=48741f9b-99fb-4ea5-a977-65a90055aad3",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fshakeshack_v4.1.7_5.PNG?alt=media&token=b5eb3cdb-ec98-4c6a-81b1-d7b17217f69b"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fendel.jpg?alt=media&token=ff559141-2766-4755-a533-dc9383d21321",
+        heading:"Endel",
+        sub:"Focus, Sleep, Relax",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fendel_v1.26_2.PNG?alt=media&token=23b10070-aa0e-448e-83f8-fa0c5305d51f",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fendel_v1.26_3.PNG?alt=media&token=ba4d4da2-fdbf-49da-ba70-bd14835d58e0",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fendel_v1.26_4.PNG?alt=media&token=6f1eea51-61ef-45fb-8736-d38758b84a79",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fendel_v1.26_5.PNG?alt=media&token=c6c6fb08-14d6-4fee-a069-fae8e5916774"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fsnapchat_v10.6.jpg?alt=media&token=d61550df-4a80-4b4b-a694-bfb7278c45c3",
+        heading:"Snapchat",
+        sub:"Real friends. Real you.",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fsnapchatv10.64.0_1.PNG?alt=media&token=ef2b93fd-b435-4500-9639-8611fc00f119",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fsnapchatv10.64.0_2.PNG?alt=media&token=44ab9252-c699-4a8e-b257-73a8cdd0237d",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fsnapchatv10.64.0_3.PNG?alt=media&token=5dcb711f-279f-46b9-b3de-0c40966ce5da",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fsnapchatv10.64.0_4.PNG?alt=media&token=dcc46f72-eadf-4d43-a975-0d54b327e2fc"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fjour.jpg?alt=media&token=14114b51-9217-419e-94b3-8dd472a1786f",
+        heading:"Jour",
+        sub:"Guided Journaling",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fjour_v1.2.8_1.PNG?alt=media&token=55de89d1-c5cf-4c6f-98ae-e2a6dec39a99",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fjour_v1.2.8_2.PNG?alt=media&token=590fb0f9-92c9-4f65-ad90-74e2c8841e4a",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fjour_v1.2.8_3.PNG?alt=media&token=ea914376-3add-4078-bac6-924064180241",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fjour_v1.2.8_4.PNG?alt=media&token=5a5e056f-7fbf-4d41-8170-72f4397613ac"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Firl.jpg?alt=media&token=d76622e7-5bfb-460d-b26f-5a094aa669a0",
+        heading:"IRL",
+        sub:"Social Calender.",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Firl_v2.0.7_1.PNG?alt=media&token=a9f8891b-5e2e-45b3-95a0-de27acc21ee0",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Firl_v2.0.7_2.PNG?alt=media&token=6191c9f3-dbc1-4185-b997-a836949a3094",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Firl_v2.0.7_3.PNG?alt=media&token=f53e861a-bc8d-486e-a5e7-d2b8ccff7812",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Firl_v2.0.7_4.PNG?alt=media&token=2f63e202-07a6-4822-a51f-395dd7dd2397"
+    },
+    {
+        logo:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/appLogos%2Fnavigator.jpg?alt=media&token=e24bb73f-d302-4e7c-9ca6-91557d6eeae6",
+        heading:"Navigator",
+        sub:"Reclaim the power of meetings",
+        disp1:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fnavigator_v1.0.4_2.PNG?alt=media&token=2afc8ae9-7352-40b4-b7d4-52fcef2687c5",
+        disp2:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fnavigator_v1.0.4_3.PNG?alt=media&token=fc00f5cd-8250-45f9-94db-e70e8d07d8cd",
+        disp3:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fnavigator_v1.0.4_4.PNG?alt=media&token=1f3160f2-51c9-48a7-a51c-16bd7db5bb41",
+        disp4:"https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fnavigator_v1.0.4_5.PNG?alt=media&token=c9aa9a35-d8fb-42ea-9a3d-e42ba4998a9b"
+    },
+    
+    
+]
+
+function display_content()
+{
+    obj.forEach(element=>{
+    var divappend=document.createElement("DIV");
+    var imgdivappend=document.createElement("DIV");
+    var headdivappend=document.createElement("DIV");
+    var subdivappend=document.createElement("DIV");
+    var topdivappend=document.createElement("DIV");
+    divappend.classList.add("space_down");
+    topdivappend.classList.add("to_float");
+    headdivappend.classList.add("heading_js1");
+    var logo=document.createElement("IMG");
+    logo.classList.add("logo_js")
+    logo.setAttribute("src",element.logo);
+
+    var heading=document.createElement("H2");
+    var headingText=document.createTextNode(element.heading);
+    heading.appendChild(headingText);
+   
+
+    var subheading=document.createElement("H6");
+    var subheadingText=document.createTextNode(element.sub);
+    subheading.appendChild(subheadingText)
+
+    var link=document.createElement("A");
+    link.setAttribute("href","#");
+
+    var img1=document.createElement("img");
+    img1.setAttribute("src",element.disp1);
+    img1.classList.add("main_img_js");
+
+    var img2=document.createElement("img");
+    img2.setAttribute("src",element.disp2);
+    img2.classList.add("main_img_js");
+
+    var img3=document.createElement("img");
+    img3.setAttribute("src",element.disp3);
+    img3.classList.add("main_img_js");
+
+    var img4=document.createElement("img");
+    img4.setAttribute("src",element.disp4);
+    img4.classList.add("main_img_js");
+
+    imgdivappend.appendChild(logo);
+    topdivappend.appendChild(imgdivappend);
+    headdivappend.appendChild(heading);
+    subdivappend.appendChild(subheading);
+    headdivappend.appendChild(subdivappend);
+    topdivappend.appendChild(headdivappend);
+    divappend.appendChild(topdivappend);
+    divappend.appendChild(img1);
+    divappend.appendChild(img2);
+    divappend.appendChild(img3);
+    divappend.appendChild(img4);
+    link.appendChild(divappend);
+    document.getElementById("list_js").appendChild(divappend);
+     } );
+
+     if(localStorage.getItem("currentUser")!==null){
+        document.getElementById("signup_main").style.display="none";
+        document.getElementById("body_signin").style.display="none";
+        document.getElementById("bodylast_signin").style.display="none";
+        document.getElementById("body_signup").style.display="none";
+        document.getElementById("body_welcome").innerHTML="Welcome"+" "+localStorage.getItem("currentUser")+":";
+     }
+    
+     
+}
+
+function displaySignup_modal()
+{
+    document.getElementById("signup_main").style.display="block";
+    document.getElementById("signin_main").style.display="none";
+}
+var modal = document.getElementById('signup_main');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function displaySignin_modal()
+{
+    document.getElementById("signin_main").style.display="block";
+    document.getElementById("signup_main").style.display="none";
+    document.getElementById("auth_id").style.display="none";
+}
+var modal = document.getElementById('signin_main');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var accounts_array=new Array();
+
+
+function addUser(){
+    var fname=document.getElementById("first_name").value;
+    var lname=document.getElementById("last_name").value;
+    var email=document.getElementById("email").value;
+    var username=document.getElementById("user_name").value;
+    var password=document.getElementById("pwd").value;
+   console.log(fname);
+    if(fname!==""&&lname!==""&&email!==""&&username!==""&&password!=="")
+    {
+
+    var account_obj={
+        f_name:fname,
+        l_name:lname,
+        e_mail:email,
+        user_name:username,
+        pass_word:password
+    }
+    localStorage.setItem(username,JSON.stringify( account_obj));
+    localStorage.setItem("currentUser",username);
+   
+
+    document.getElementById("signup_main").style.display="none";
+    document.getElementById("body_signin").style.display="none";
+    document.getElementById("bodylast_signin").style.display="none";
+    document.getElementById("body_signup").style.display="none";
+    document.getElementById("body_welcome").innerHTML="Welcome"+" "+username+":";
+}
+else{
+    alert("All Fields Required");
+}
+
+    document.getElementById("first_name").value="";
+    document.getElementById("last_name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("user_name").value="";
+    document.getElementById("pwd").value="";
+}
+
+function verifyUser(){
+    
+    var id=document.getElementById("signin_id").value;
+    var fetchedData=JSON.parse( localStorage.getItem(id));
+    var pass=document.getElementById("signin_pwd").value;
+    localStorage.setItem("currentUser",id);
+    if(fetchedData.pass_word===pass){
+           
+        document.getElementById("body_welcome").innerHTML="Welcome"+" "+fetchedData.user_name;
+             document.getElementById("signin_main").style.display="none";
+            document.getElementById("body_signin").style.display="none";
+            document.getElementById("bodylast_signin").style.display="none";
+            document.getElementById("body_signup").style.display="none";
+         
+    }
+    else{
+        document.getElementById("signin_main").style.display="block";
+        document.getElementById("auth_id").style.display="block";
+    }
+}
+
